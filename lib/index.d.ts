474dee20b207d488eb4490d8f55d6e5ef0f81471
ChokoJoestar <mohamedboulidam680@gmail.com>
2023-01-20 12:34:19 +0100

    First feature

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,9 @@
-declare const MyFonction: () => void;
-export default MyFonction;
+/**
+ *
+ * @param name - Name
+ * @param wantLog - Boolean
+ * @returns
+ */
+declare function isChokoJoestar(name: string, wantLog: boolean): boolean | undefined;
+export default isChokoJoestar;
 //# sourceMappingURL=index.d.ts.map
